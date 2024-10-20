89895208c0c45ac7ca380e9cbd464b7073c575f8
Robin de Villiers <robindevilliers@me.com>
2024-10-20 21:18:59 +0100

    adding extra files.

---
+++ dist/rendering/renderers/ContinueOperationButtonRenderer.d.ts
@@ -0,0 +1,9 @@
+import { Renderer } from "../Renderer";
+import { Element } from "../../xml-parser";
+import Properties from "../Properties";
+import RenderingEngine from "../RenderingEngine";
+import { Substitutions } from "../Substitutions";
+export default class ContinueOperationButtonRenderer implements Renderer {
+    accept(name: string): boolean;
+    render(element: Element, classMappings: Properties, renderingEngine: RenderingEngine, substitutions: Substitutions, parent: Element | undefined): string;
+}

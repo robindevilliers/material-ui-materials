9082d1560da1b79e50c7e4835b9a2f97ad1d3707
Robin de Villiers <robindevilliers@me.com>
2025-08-25 21:28:46 +0100

    reworked tokenization, and added confirmation page.

---
+++ customize.js
@@ -0,0 +1,169 @@
+/*
+    This file will go through a css file and replace tokens with customized values.
+    The source css should be the tokenized output of the sass build that the Maximillian platform will use.
+    The Maximillian platform will do what this script is doing but on the fly with whatever tokens have been
+    configured within settings.
+ */
+
+const {mix, toHex, color, darken, rgba, darkenA, toRgba, colorA, colorYiq, rgb, colorYiqIf} = require("./utils");
+
+const fs = require('fs');
+
+const themeColorMappings = {
+    "COLOR_DEFAULT": "#808080",
+    "COLOR_PRIMARY": "#81b1cc",
+    "COLOR_SUCCESS": "#c1e1c1",
+    "COLOR_WARNING": "#ff964f",
+    "COLOR_DANGER": "#ff6961",
+    "COLOR_INFO": "#a9c8c0",
+    "COLOR_MUTED": "#D3D3D3",
+    "COLOR_SECONDARY": "#D3D3D3",
+};
+
+function themeColor(name) {
+    return themeColorMappings[name];
+}
+
+const file = 'materials/public/css/main.css';
+
+console.log('> running customization');
+
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
+
+fs.readFile(inputFile, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    data = data.replaceAll("\"rgba(black, 0)\"", toRgba(colorA("black", 0)));
+
+    data = replaceForThemeColor(data, 'COLOR_DEFAULT', themeColor('COLOR_DEFAULT'));
+    data = replaceForThemeColor(data, 'COLOR_PRIMARY', themeColor('COLOR_PRIMARY'));
+    data = replaceForThemeColor(data, 'COLOR_SECONDARY', themeColor('COLOR_SECONDARY'));
+    data = replaceForThemeColor(data, 'COLOR_SUCCESS', themeColor('COLOR_SUCCESS'));
+    data = replaceForThemeColor(data, 'COLOR_WARNING', themeColor('COLOR_WARNING'));
+    data = replaceForThemeColor(data, 'COLOR_INFO', themeColor('COLOR_INFO'));
+    data = replaceForThemeColor(data, 'COLOR_DANGER', themeColor('COLOR_DANGER'));
+    data = replaceForThemeColor(data, 'COLOR_MUTED', themeColor('COLOR_MUTED'));
+    data = replaceForThemeColor(data, 'COLOR_BLACK', 'black');
+    data = replaceForThemeColor(data, 'COLOR_WHITE', 'white');
+    const result = data
+
+    .replaceAll("\"darken(rgba(0, 0, 0, 0.075),5%)\"", toRgba(darkenA(colorA('black', 0.075), 5)))
+    .replaceAll("\"color-yiq(#28a745,#212529,#fff)\"", toHex(colorYiq(color("#28a745"), color("#212529"), color("#fff"))))
+    .replaceAll("\"rgba(#28a745, 0.9)\"", toRgba(color("#28a745"), 0.9))
+    .replaceAll("\"rgba(#28a745, 0.25)\"", toRgba(color("#28a745"), 0.25))
+    .replaceAll("\"color-yiq(#dc3545,#212529,#fff)\"", toHex(colorYiq(color('#dc3545'), color('#212529'), color('#fff'))))
+    .replaceAll("\"rgba(#dc3545, 0.9)\"", toRgba(colorA('#dc3545', 0.9)))
+    .replaceAll("\"rgba(#dc3545, 0.25)\"", toRgba(colorA('#dc3545', 0.25)))
+    .replaceAll("\"rgba(white, 0.15)\"", toRgba(colorA('white', 0.15)))
+    .replaceAll("\"darken(rgb(247.35, 247.35, 247.35),5%)\"", toHex(darken(rgb(247.35, 247.35, 247.35), 5)))
+    .replaceAll("\"color-yiq(lavender,#212529,#fff)\"", toHex(colorYiq(color("lavender"), color("#212529"), color("#fff"))))
+
+    .replaceAll("\"rgba(0, 0, 0, 0.14)\"", toRgba(color("black"), 0.14))
+    .replaceAll("\"rgba(0, 0, 0, 0.12)\"", toRgba(color("black"), 0.12))
+    .replaceAll("\"rgba(0, 0, 0, 0.125)\"", toRgba(color("black"), 0.125))
+    .replaceAll("\"rgba(0, 0, 0, 0.2)\"", toRgba(color("black"), 0.2))
+    .replaceAll("\"rgba(255, 255, 255, 0.15)\"", toRgba(color("white"), 0.15))
+    .replaceAll("\"rgba(0, 0, 0, 0.075)\"", toRgba(color("black"), 0.075))
+    .replaceAll("\"rgba(0, 0, 0, 0.05)\"", toRgba(color("black"), 0.05))
+    .replaceAll("\"rgba(57, 76, 96, 0.15)\"", toRgba(rgba(57, 76, 96, 0.15)))
+
+    .replaceAll("\"color-yiq(#a9c8c0,#212529,#fff)\"", toHex(colorYiq(color("#a9c8c0"), color("#212529"), color("#fff"))))
+    .replaceAll("\"color-yiq(#c1e1c1,#212529,#fff)\"", toHex(colorYiq(color("#c1e1c1"), color("#212529"), color("#fff"))))
+
+    //color picker mappings
+    .replaceAll("\"rgba(64, 87, 109, 0.07)\"", toRgba(rgba(64, 87, 109, 0.07)))
+    .replaceAll("\"rgba(242, 13, 13, 0.8)\"", toRgba(rgba(242, 13, 13, 0.8)))
+    .replaceAll("\"rgba(230, 26, 26, 0.6)\"", toRgba(rgba(230, 26, 26, 0.6)))
+    .replaceAll("\"rgba(204, 51, 51, 0.4)\"", toRgba(rgba(204, 51, 51, 0.4)))
+    .replaceAll("\"rgba(166, 89, 89, 0.2)\"", toRgba(rgba(166, 89, 89, 0.2)))
+    .replaceAll("\"rgba(242, 185, 13, 0.8)\"", toRgba(rgba(242, 185, 13, 0.8)))
+    .replaceAll("\"rgba(230, 179, 26, 0.6)\"", toRgba(rgba(230, 179, 26, 0.6)))
+    .replaceAll("\"rgba(204, 166, 51, 0.4)\"", toRgba(rgba(204, 166, 51, 0.4)))
+    .replaceAll("\"rgba(166, 147, 89, 0.2)\"", toRgba(rgba(166, 147, 89, 0.2)))
+    .replaceAll("\"rgba(128, 242, 13, 0.8)\"", toRgba(rgba(128, 242, 13, 0.8)))
+    .replaceAll("\"rgba(128, 230, 26, 0.6)\"", toRgba(rgba(128, 230, 26, 0.6)))
+    .replaceAll("\"rgba(128, 204, 51, 0.4)\"", toRgba(rgba(128, 204, 51, 0.4)))
+    .replaceAll("\"rgba(128, 166, 89, 0.2)\"", toRgba(rgba(128, 166, 89, 0.2)))
+    .replaceAll("\"rgba(13, 242, 70, 0.8)\"", toRgba(rgba(13, 242, 70, 0.8)))
+    .replaceAll("\"rgba(26, 230, 77, 0.6)\"", toRgba(rgba(26, 230, 77, 0.6)))
+    .replaceAll("\"rgba(51, 204, 89, 0.4)\"", toRgba(rgba(51, 204, 89, 0.4)))
+    .replaceAll("\"rgba(89, 166, 108, 0.2)\"", toRgba(rgba(89, 166, 108, 0.2)))
+    .replaceAll("\"rgba(13, 242, 242, 0.8)\"", toRgba(rgba(13, 242, 242, 0.8)))
+    .replaceAll("\"rgba(26, 230, 230, 0.6)\"", toRgba(rgba(26, 230, 230, 0.6)))
+    .replaceAll("\"rgba(51, 204, 204, 0.4)\"", toRgba(rgba(51, 204, 204, 0.4)))
+    .replaceAll("\"rgba(89, 166, 166, 0.2)\"", toRgba(rgba(89, 166, 166, 0.2)))
+    .replaceAll("\"rgba(13, 70, 242, 0.8)\"", toRgba(rgba(13, 70, 242, 0.8)))
+    .replaceAll("\"rgba(26, 77, 230, 0.6)\"", toRgba(rgba(26, 77, 230, 0.6)))
+    .replaceAll("\"rgba(51, 89, 204, 0.4)\"", toRgba(rgba(51, 89, 204, 0.4)))
+    .replaceAll("\"rgba(89, 108, 166, 0.2)\"", toRgba(rgba(89, 108, 166, 0.2)))
+    .replaceAll("\"rgba(128, 13, 242, 0.8)\"", toRgba(rgba(128, 13, 242, 0.8)))
+    .replaceAll("\"rgba(128, 26, 230, 0.6)\"", toRgba(rgba(128, 26, 230, 0.6)))
+    .replaceAll("\"rgba(128, 51, 204, 0.4)\"", toRgba(rgba(128, 51, 204, 0.4)))
+    .replaceAll("\"rgba(128, 89, 166, 0.2)\"", toRgba(rgba(128, 89, 166, 0.2)))
+    .replaceAll("\"rgba(242, 13, 185, 0.8)\"", toRgba(rgba(242, 13, 185, 0.8)))
+    .replaceAll("\"rgba(230, 26, 179, 0.6)\"", toRgba(rgba(230, 26, 179, 0.6)))
+    .replaceAll("\"rgba(204, 51, 166, 0.4)\"", toRgba(rgba(204, 51, 166, 0.4)))
+    .replaceAll("\"rgba(166, 89, 147, 0.2)\"", toRgba(rgba(166, 89, 147, 0.2)))
+
+
+    .replace(/FONT_TITLE_PRIMARY_FAMILY/g, 'Liberation Sans, sans-serif')
+    .replace(/FONT_TITLE_SECONDARY_FAMILY/g, 'Liberation Sans, sans-serif')
+    .replace(/FONT_TEXT_FAMILY/g, 'Liberation Sans, sans-serif')
+    .replace(/FONT_LABEL_FAMILY/g, 'Liberation Sans, sans-serif')
+    .replace(/FONT_EXHIBIT_FAMILY/g, 'Liberation Mono, monospace')
+
+    .replace(/FONT_TITLE_PRIMARY_SIZE/g, 'initial')
+    .replace(/FONT_TITLE_SECONDARY_SIZE/g, 'initial')
+    .replace(/FONT_TEXT_SIZE/g, 'initial')
+    .replace(/FONT_LABEL_SIZE/g, 'initial')
+    .replace(/FONT_EXHIBIT_SIZE/g, 'initial')
+
+    .replace(/FONT_TITLE_PRIMARY_WEIGHT/g, 'inherit')
+    .replace(/FONT_TITLE_SECONDARY_WEIGHT/g, 'inherit')
+    .replace(/FONT_TEXT_WEIGHT/g, 'inherit')
+    .replace(/FONT_LABEL_WEIGHT/g, 'inherit')
+    .replace(/FONT_EXHIBIT_WEIGHT/g, 'inherit')
+
+    .replace(/FONT_TITLE_PRIMARY_STYLE/g, 'inherit')
+    .replace(/FONT_TITLE_SECONDARY_STYLE/g, 'inherit')
+    .replace(/FONT_TEXT_STYLE/g, 'inherit')
+    .replace(/FONT_LABEL_STYLE/g, 'inherit')
+    .replace(/FONT_EXHIBIT_STYLE/g, 'inherit')
+
+    .replace(/FONT_TITLE_PRIMARY_DECORATION/g, 'inherit')
+    .replace(/FONT_TITLE_SECONDARY_DECORATION/g, 'inherit')
+    .replace(/FONT_TEXT_DECORATION/g, 'inherit')
+    .replace(/FONT_LABEL_DECORATION/g, 'inherit')
+    .replace(/FONT_EXHIBIT_DECORATION/g, 'inherit')
+    ;
+
+    fs.writeFile(outputFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
+
+
+function replaceForThemeColor(data, colorTheme, colorCode) {
+    return data
+    .replaceAll(`"mix(white,${colorTheme},72%)"`, toHex(mix(color('white'), color(colorCode), 72)))
+    .replaceAll(`"mix(white,${colorTheme},48%)"`, toHex(mix(color('white'), color(colorCode), 48)))
+    .replaceAll(`"darken(mix(white,${colorTheme},72%),5%)"`, toHex(darken(mix(color('white'), color(colorCode), 72), 5)))
+    .replaceAll(`"color-yiq(${colorTheme},#212529,#fff)"`, toHex(colorYiq(color(colorCode), color('#212529'), color('#fff'))))
+    .replaceAll(`"color-yiq(darken(${colorTheme},7.5%),#212529,#fff)"`, toHex(colorYiq(darken(color(colorCode), 7.5), color('#212529'), color('#fff'))))
+    .replaceAll(`"darken(${colorTheme},7.5%)"`, toHex(darken(color(colorCode), 7.5)))
+    .replaceAll(`"darken(${colorTheme},10%)"`, toHex(darken(color(colorCode), 10)))
+    .replaceAll(`"rgba(mix(color-yiq(${colorTheme},#212529,#fff),${colorTheme},15%), 0.5)"`, toRgba(mix(colorYiq(color(colorCode), color("#212529"), color("#fff")), color(colorCode), 15), 0.5))
+    .replaceAll(`"color-yiq(darken(${colorTheme},10%),#212529,#fff)"`, toHex(colorYiq(darken(color(colorCode), 10), color("#212529"), color("#fff"))))
+    .replaceAll(`"darken(${colorTheme},12.5%)"`, toHex(darken(color(colorCode), 12.5)))
+    .replaceAll(`"mix(black,${colorTheme},48%)"`, toHex(mix(color("black"), color(colorCode), 48)))
+    .replaceAll(`"rgba(${colorTheme}, 0.5)"`, toRgba(color(colorCode), 0.5))
+    .replaceAll(`"mix(white,${colorTheme},80%)"`, toHex(mix(color("white"), color(colorCode), 80)))
+    .replaceAll(`"darken(mix(black,${colorTheme},48%),10%)"`, toHex(darken(mix(color("black"), color(colorCode), 48), 10)))
+    .replaceAll(`"color-yiq-if(${colorTheme},1,-3)"`, toHex(colorYiqIf(color(colorCode), 1, -3)))
+    .replaceAll(colorTheme, colorCode)
+    ;
+
+}
---
+++ utils.js
@@ -0,0 +1,240 @@
+const namedColors = {
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#808080',
+    lightgray: '#D3D3D3',
+    darkgray: '#A9A9A9',
+    silver: '#C0C0C0',
+    lavender: '#E6E6FA'
+};
+
+module.exports = {mix, toHex, color, darken, fromHsl, toHsl, rgba, rgb, darkenA, toRgba, colorA, colorYiq, colorYiqIf}
+
+function rgb(r, g, b) {
+    return {
+        red: r, green: g, blue: b
+    };
+}
+
+function rgba(r, g, b, a) {
+    return {
+        red: r, green: g, blue: b, alpha: a
+    };
+}
+
+function colorA(value, alpha) {
+    const col = color(value);
+    col.alpha = alpha;
+    return col;
+}
+
+function color(value) {
+    if (value === null || value === undefined) {
+        return value;
+    }
+
+    if (value.charAt(0) === '#') {
+        if (value.length === 4) {
+            return {
+                red: Number('0x' + value.charAt(1) + value.charAt(1)),
+                green: Number('0x' + value.charAt(2) + value.charAt(2)),
+                blue: Number('0x' + value.charAt(3) + value.charAt(3)),
+            }
+        } else if (value.length === 7) {
+            return {
+                red: Number('0x' + value.substring(1, 3)),
+                green: Number('0x' + value.substring(3, 5)),
+                blue: Number('0x' + value.substring(5, 7)),
+            }
+        }
+    } else if (value.startsWith('rgb(')) {
+
+        const argString = value.substring(4, value.length);
+
+        const args = argString.split(',').map(s => s.trim());
+
+        return {
+            red: Number(args[0]),
+            green: Number(args[1]),
+            blue: Number(args[2]),
+        }
+
+    } else {
+        return color(namedColors[value]);
+    }
+}
+
+function toHex(color) {
+    const rh = Math.round(color.red).toString(16);
+    const gh = Math.round(color.green).toString(16);
+    const bh = Math.round(color.blue).toString(16);
+
+    return "#" + rh + gh + bh;
+}
+
+// function toHexA(color) {
+//
+//     const rh = Math.round(color.red).toString(16);
+//     const gh = Math.round(color.green).toString(16);
+//     const bh = Math.round(color.blue).toString(16);
+//     const ah = Math.round(color.alpha).toString(16); //NOT CORRECT
+//
+//     return "#" + rh + gh + bh + ah;
+// }
+
+function toRgba(color, alpha) {
+    return `rgba(${color.red},${color.green},${color.blue},${alpha ? alpha : color.alpha})`;
+}
+
+function toHsl(color) {
+    // Then to HSL
+    let {red: r, green: g, blue: b} = color;
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+    if (delta === 0)
+        h = 0;
+    else if (cmax === r)
+        h = ((g - b) / delta) % 6;
+    else if (cmax === g)
+        h = (b - r) / delta + 2;
+    else
+        h = (r - g) / delta + 4;
+
+    h = h * 60;
+
+    if (h < 0)
+        h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(10);
+    l = +(l * 100).toFixed(10);
+
+    return {
+        hue: h,
+        sat: s,
+        lum: l
+    };
+}
+
+function toHslA(color) {
+    const hsl = toHsl(color);
+    hsl.alpha = color.alpha;
+    return hsl;
+}
+
+
+function darkenA(color, amount) {
+    const hsl = toHslA(color);
+    hsl.lum = Math.max(hsl.lum - amount, 0);
+    return fromHslA(hsl);
+}
+
+function darken(color, amount) {
+    const hsl = toHsl(color);
+    hsl.lum = Math.max(hsl.lum - amount, 0);
+    return fromHsl(hsl);
+}
+
+function fromHsl(hsl) {
+    let {hue: h, sat: s, lum: l} = hsl;
+
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+
+    if (0 <= h && h < 60) {
+        r = c;
+        g = x;
+        b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x;
+        g = c;
+        b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0;
+        g = c;
+        b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0;
+        g = x;
+        b = c;
+    } else if (240 <= h && h < 300) {
+        r = x;
+        g = 0;
+        b = c;
+    } else if (300 <= h && h < 360) {
+        r = c;
+        g = 0;
+        b = x;
+    }
+    r = ((r + m) * 255);
+    g = ((g + m) * 255);
+    b = ((b + m) * 255);
+
+    return {
+        red: r, blue: b, green: g
+    };
+}
+
+function fromHslA(hsl) {
+    const color = fromHsl(hsl);
+    color.alpha = hsl.alpha;
+    return color;
+}
+
+function mix(color1, color2, weight) {
+
+    function mixP(color1, color2, weight) {
+        return Math.max(color2 + (color1 - color2) * (weight / 100.0), 0);
+    }
+
+    return {
+        red: mixP(color1.red, color2.red, weight),
+        green: mixP(color1.green, color2.green, weight),
+        blue: mixP(color1.blue, color2.blue, weight)
+    }
+}
+
+
+function colorYiq(color, dark, light) {
+    if ((color.red * 299 + color.green * 587 + color.blue * 114) * 0.001 >= 150) {
+        return dark;
+    } else {
+        return light;
+    }
+}
+
+function colorYiqIf(color, trueValue, falseValue) {
+    if ((color.red * 299 + color.green * 587 + color.blue * 114) * 0.001 >= 150) {
+        return themeColorLevel(color, trueValue);
+    } else {
+        return themeColorLevel(color, falseValue);
+    }
+}
+
+function themeColorLevel(colorCode, level) {
+    const base = level > 0 ? color('black') : color('white');
+    return mix(base, colorCode, level * 8)
+}
+
+
+
+
+
